4f9119b5a4c31d1af362e6e2266b1c1b983b930f
Omer <omer.sidd93@gmail.com>
2023-07-26 00:42:06 +0500

    api modified 3

--- api.py
+++ api.py
@@ -35,7 +35,7 @@
 
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from bson import ObjectId
+from bson.objectid import ObjectId
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 
